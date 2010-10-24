9302b69073aa716193bd47dd173c907db3518ddb
LindseyB <hazardous.narayan@gmail.com>
2010-10-24 16:42:07 -0400

    fixed various bugs and shitgit add .git add .

--- backupDancer.d
+++ backupDancer.d
@@ -5,8 +5,8 @@ import tango.math.random.Random;
 import animatedAsciiSprite;
 
 class BackupDancer {
-	enum Animate { DOWN, JUMP, LEFT, MOONWALK, RIGHT, UP, YMCA }
-	AnimatedAsciiSprite[7] _animations;
+	enum Animate { DOWN, JUMP, LEFT, MOONWALK, RIGHT, UP, YMCA, THRUSTLEFT, THRUSTRIGHT, DISCO }
+	AnimatedAsciiSprite[10] _animations;
 	Animate _curAnimation;
 
 	this(int x, int y) {
@@ -17,6 +17,9 @@ class BackupDancer {
 		_animations[Animate.RIGHT] = new AnimatedAsciiSprite("graphics/man-right.txt", null, true, true, x, y);
 		_animations[Animate.UP] = new AnimatedAsciiSprite("graphics/man-up.txt", null, true, true, x, y);
 		_animations[Animate.YMCA] = new AnimatedAsciiSprite("graphics/man-ymca.txt", null, true, true, x, y);
+		_animations[Animate.THRUSTLEFT] = new AnimatedAsciiSprite("graphics/man-thrust-left.txt", null, true, true, x, y);
+		_animations[Animate.THRUSTRIGHT] = new AnimatedAsciiSprite("graphics/man-thrust-right.txt", null, true, true, x, y);
+		_animations[Animate.DISCO] = new AnimatedAsciiSprite("graphics/man-disco.txt", null, true, true, x, y);
 	}
 
 	void setCurAnimation(Animate animation) {
@@ -25,7 +28,7 @@ class BackupDancer {
 
 	void animate() {
 		if(_animations[_curAnimation]._frame == _animations[_curAnimation]._animation.length - 1){
-			int random = rand.uniformR(7);
+			int random = rand.uniformR(10);
 			
 			switch(random) {
 				case 0: _curAnimation = Animate.DOWN;
@@ -37,6 +40,14 @@ class BackupDancer {
 				case 3: _curAnimation = Animate.RIGHT;
 						break;
 				case 4: _curAnimation = Animate.UP;
+						break;
+				case 5: _curAnimation = Animate.THRUSTLEFT;
+						break;
+				case 6: _curAnimation = Animate.THRUSTRIGHT;
+						break;
+				case 7: _curAnimation = Animate.DISCO;
+						break;
+				case 8: _curAnimation = Animate.LEFT;
 						break;
 				default: _curAnimation = Animate.YMCA;
 			}	
