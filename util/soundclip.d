b5ee0e3e70684de526022dd4d0d3867abb557658
James <transmethyl@gmail.com>
2010-10-24 11:24:19 +0800

    sound, maybe?

---
+++ util/soundclip.d
@@ -0,0 +1,63 @@
+module util.soundclip;
+
+import tango.stdc.posix.unistd;
+import tango.stdc.posix.signal;
+import tango.stdc.stringz;
+
+class SoundClip{
+private:
+	char[] exe;
+	char[] filename;
+	pid_t pid;
+	bool paused;
+
+public:
+	this(char[] fn){
+		filename = fn;
+		pid = 0;
+	}
+
+	bool start(){
+		if(pid != 0){return false;}
+
+		if((pid = fork()) == 0){
+			char[] temp = exe ~ filename;
+
+			execlp(toStringz(exe), toStringz(filename), cast(void*)null);
+
+		}else{
+			// ???
+			if(pid > 0){
+			
+				return true;
+			}else{
+				return false;
+			}
+		}
+	}
+
+	bool stop(){
+		if(pid == 0){return false;}
+
+		if(kill(pid, SIGKILL) != 0){return false;}
+
+		//XXX: wait for child zombie
+
+		return true;
+	}
+
+	bool pause(){
+		if(pid == 0 || paused){return false;}
+		return signal(SIGSTP);
+	}
+
+	void unpause(){
+		if(pid == 0 || !paused){return false;}
+		return signal(SIGCONT);
+	}
+
+
+	bool signal(int sig){
+		return (kill(pid, sig) == 0);
+	}
+}
