c14299ef11fac4b9cef181e1dfa64cefd147bb51
James <transmethyl@gmail.com>
2010-10-24 10:12:17 -0400

    random arrows... almost

--- arrowSection.d
+++ arrowSection.d
@@ -14,10 +14,16 @@ class ArrowSection {
 	
 	double sleep;
 	
+	ubyte _input;
+
 	this() {
 		_frame = new AsciiSprite("graphics/arrow-frame.txt", null, true, 60, 0);
 		hit = new AsciiSprite("graphics/hit_it_now_bar.txt", null, true, 62, 5);
-		//l = new AsciiSprite("graphics/arrow-frame.txt", null, true, 60, 0);
+
+		l = new AsciiSprite("graphics/arrow-left.txt", null, true, 63, 0);
+		d = new AsciiSprite("graphics/arrow-down.txt", null, true, 71, 0);
+		u = new AsciiSprite("graphics/arrow-up.txt", null, true, 79, 0);
+		r = new AsciiSprite("graphics/arrow-right.txt", null, true, 87, 0);
 	}
 	
 	void draw() {
@@ -43,32 +49,39 @@ class ArrowSection {
 			sleep = beats[0].period;
 		}
 
-		offset++;
-		offset %= 4;
+		offset--;
+		if(offset < 0){offset = 3;}
 
 
 		// Draw
 		for(int i = 0; i < beats.length; i++){
 
 			ubyte arrows = beats[i].arrows;
-		
+
+			int row = 1 + offset + 4*i;
+
 			// draw left arrow
 			if(arrows & 1){
-				
+				l.setY(row);
+				l.drawSprite();
 			}
 
 			// draw right arrow
 			if(arrows & 2){
-
+				r.setY(row);
+				r.drawSprite();
 			}
 
 			// draw up arrow
 			if(arrows & 4){
-
+				u.setY(row);
+				u.drawSprite();
 			}
 
 			// draw down arrow
 			if(arrows & 8){
+				d.setY(row);
+				d.drawSprite();
 
 			}
 		}
