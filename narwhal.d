0419f6b2adbecd09f132fcae51fba6537d4ff2b0
LindseyB <hazardous.narayan@gmail.com>
2010-10-24 13:20:24 -0400

    More animations for the narwhal.

--- narwhal.d
+++ narwhal.d
@@ -6,14 +6,16 @@ import animatedAsciiSprite;
 
 
 class Narwhal {
-	enum Animate { DANCE, BREAKDANCE }
-	AnimatedAsciiSprite[2] _animations;
+	enum Animate { DANCE, BREAKDANCE, FLYING, SNOOPY }
+	AnimatedAsciiSprite[4] _animations;
 	Animate _curAnimation;
 
 	this() {
 		// init animations
 		_animations[Animate.BREAKDANCE] = new AnimatedAsciiSprite("graphics/narwhal-breakdance.txt", null, true, true, 10, 5);
 		_animations[Animate.DANCE] = new AnimatedAsciiSprite("graphics/narwhal-dance.txt", null, true, true, 10, 5);
+		_animations[Animate.FLYING] = new AnimatedAsciiSprite("graphics/narwhal-flying.txt", null, true, true, 10, 5);
+		_animations[Animate.SNOOPY] = new AnimatedAsciiSprite("graphics/narwhal-snoopy.txt", null, true, true, 10, 5);
 	}
 
 	void setCurAnimation(Animate animation) {
@@ -26,10 +28,19 @@ class Narwhal {
 			if(_curAnimation == Animate.BREAKDANCE){
 				_curAnimation = Animate.DANCE;
 			} else {
-				int random = rand.uniformR(10);
+				int random = rand.uniformR(20);
 
-				if(random == 5){
-					_curAnimation = Animate.BREAKDANCE;
+				if(random <= 2){
+					random = rand.uniformR(3);
+
+					switch(random) {
+						case 0: _curAnimation = Animate.BREAKDANCE;
+								break;
+						case 1: _curAnimation = Animate.FLYING;
+								break;
+						default: _curAnimation = Animate.SNOOPY;
+								 break;
+					}
 				}
 			}
 
