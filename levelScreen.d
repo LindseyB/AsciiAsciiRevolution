f76bcd79beafeeb6d2c33a12273228a829f303f0
LindseyB <hazardous.narayan@gmail.com>
2010-10-24 06:35:42 -0400

    The frame for the arrow section is now being drawn.

--- levelScreen.d
+++ levelScreen.d
@@ -5,18 +5,22 @@ import ncurses;
 import tango.io.Stdout; 
 
 import dataScore;
+import arrowSection;
 
 class LevelScreen {
 	DataScore _score;
+	ArrowSection _arrowSect;
 	bool _playing;
 
 	this(char[] title) {
 		_score = new DataScore(title);
+		_arrowSect = new ArrowSection();
 		_playing = true;
 	}
 
 	void draw() {
 		move(0,0);
 		_score.draw();
+		_arrowSect.draw();
 	}	
 }
